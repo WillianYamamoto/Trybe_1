79d20f8b82848f1f1c88e255f0ba5579b4e219aa
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 20:15:53 -0300

    Versão_2

--- Palindromo/Program.cs
+++ Palindromo/Program.cs
@@ -6,25 +6,51 @@ using System.Linq;
 //1 - Pedir para inserir uma palavra (STRING) - OK
 //2 - Ler a palavra inserida - OK
 //3 - Inverter a palavra - OK 
-//4 - Ver se é um palíndromo 
-//5 - Melhorar UX/UI
-//Detalhes: O programa deve ignorar espaços e considerar maiúsculas e minúsculas como iguais.
+//4 - Ver se é um palíndromo - OK 
+//5 - Melhorar UX/UI - OK
+//Detalhes: O programa deve ignorar espaços e considerar maiúsculas e minúsculas como iguais. - OK
 
-
+// Variáveis 
 Console.Clear();
 string texto;
 string textoinvertido;
 string texto_sem_espaco;
+var menu = 0;
 
-Console.WriteLine("Digite a palavra a ser verificada como um Palíndromo (quando uma palavra é lida igual de trás para frente)");
- texto = Console.ReadLine();
+//Menu para escolher se quer ficar fazendo verificação de palíndromo
+while (menu != 2)
+{
+    Console.WriteLine("Escolha uma opção:\n");
+    Console.WriteLine("1: Verificar se é Palíndromo (quando uma palavra é lida igual de trás para frente)\n2:Fechar Programa");
 
-//texto_sem_espaco = new string(texto.Where(c => !char.IsWhiteSpace(c)).ToArray());
-textoinvertido = new string(texto.Reverse().ToArray());
+    if (!int.TryParse(Console.ReadLine(), out menu) || menu < 1 || menu > 2) // Caso escolha um número que não seja válido 
+    {
+        Console.WriteLine("\n Digite um número válido!!!!!\n");
+        Console.WriteLine("\nPressione qualquer tecla para continuar");
+        Console.ReadKey();
+        Console.Clear();
+        continue;
+    }
 
-if (texto == textoinvertido)
-Console.WriteLine("O texto digitado é um Palíndromo");
-else Console.WriteLine("O texto digitado não é um palindromo");
+    if (menu == 1)
+    {
+        Console.WriteLine("Digite a palavra a ser verificada como um Palíndromo (quando uma palavra é lida igual de trás para frente)");
+        texto = Console.ReadLine();
+        texto_sem_espaco = new string(texto.ToUpper().Where(c => !char.IsWhiteSpace(c)).ToArray());
+        textoinvertido = new string(texto_sem_espaco.ToUpper().Reverse().ToArray());
 
-//Console.WriteLine($"\nA palavra digitada é: {texto}");
-//Console.WriteLine($"\nA palavra invertida é: {textoinvertido}");
+        if (texto_sem_espaco == textoinvertido)
+            Console.WriteLine($"O texto digitado: {texto} é um Palíndromo");
+        else Console.WriteLine($"O texto digitado: {texto} não é um palindromo");
+    }
+    else if (menu == 2) //Opção de fechar o programa 
+    {
+        Console.WriteLine("Programa finalizado\n");
+    }
+    if (menu != 2) // Para deixar a UI/UX melhor
+    {
+        Console.WriteLine("\nPressione qualquer tecla para continuar");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
