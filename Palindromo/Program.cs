d7804c5af8938363c069a243fe39438062dd0684
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 10:50:44 -0300

    Versão_1

--- Palindromo/Program.cs
+++ Palindromo/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex do Palíndromo (quando uma palavra é lida igual de trás para frente)
+//Etapas:
+//1 - Pedir para inserir uma palavra (STRING)
+//2 - Ler a palavra inserida
+//3 - Ver se é um palíndromo 
+//Detalhes: O programa deve ignorar espaços e considerar maiúsculas e minúsculas como iguais.
