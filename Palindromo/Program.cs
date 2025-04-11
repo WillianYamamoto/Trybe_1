ab3f8fb6a80f2751cebdb24ece4831fb3629cba4
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 11:07:59 -0300

    Versão_1

--- Palindromo/Program.cs
+++ Palindromo/Program.cs
@@ -1,6 +1,15 @@
-﻿// Ex do Palíndromo (quando uma palavra é lida igual de trás para frente)
+﻿using System;
+using System.Collections.Generic;
+// Ex do Palíndromo (quando uma palavra é lida igual de trás para frente)
 //Etapas:
-//1 - Pedir para inserir uma palavra (STRING)
+//1 - Pedir para inserir uma palavra (STRING) - OK
 //2 - Ler a palavra inserida
 //3 - Ver se é um palíndromo 
+//4 - Melhorar UX/UI
 //Detalhes: O programa deve ignorar espaços e considerar maiúsculas e minúsculas como iguais.
+
+string palavra;
+Console.WriteLine("Digite a palavra a ser verificada como um Palíndromo (quando uma palavra é lida igual de trás para frente)");
+ palavra = Console.ReadLine();
+
+Console.WriteLine($"\nA palavra digitada é: {palavra}");
