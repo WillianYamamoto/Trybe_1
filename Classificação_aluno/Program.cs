b8dc6b3a2d9c84e4e560c6c1189789a7c8deb441
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 21:04:36 -0300

    Versão_2

--- Classificação_aluno/Program.cs
+++ Classificação_aluno/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crie uma lista que pega o nome e nota dos alunos
+//O programa deve calcular a média da turma e exibir as pessoas com nota acima da média 
