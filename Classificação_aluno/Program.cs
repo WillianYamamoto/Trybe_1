27ef4aacc01d5926fa8e19f736c26f50da77df11
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 21:13:49 -0300

    Versão_2

--- Classificação_aluno/Program.cs
+++ Classificação_aluno/Program.cs
@@ -1,2 +1,8 @@
 ﻿//Crie uma lista que pega o nome e nota dos alunos
 //O programa deve calcular a média da turma e exibir as pessoas com nota acima da média 
+// Etapas:
+//1 - Crie uma lista que pega o nome dos alunos
+//2 - Crie uma lista qeu pega as notas dos alunos
+//3 - Calcular qual a média desses alunos
+//4 - Exibir a média
+//5 - Exibir alunos com nota acima da média
