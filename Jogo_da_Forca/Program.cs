3c99df1c814d2e205842439c04582ca04d0a5f9a
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-12 20:40:22 -0300

    Versão_3

--- Jogo_da_Forca/Program.cs
+++ Jogo_da_Forca/Program.cs
@@ -29,9 +29,48 @@ string palavra_secreta = banco_palavras[random.Next(banco_palavras.Count)];//Par
 
 
 char[] letras_underscore = new char[palavra_secreta.Length];
-int vidas = 5;
+int vidas = 0;
+int dificuldade = 0;
 bool ganhou = false;
 
+
+
+
+while (vidas == 0)//Para rodar o menu de opção de vidas 
+{
+    Console.WriteLine("Antes de iniciar escolha o nível de dificuldade:\n1 - fácil (10 tentativas)\n2 - Médio (8 tentativas)\n3 - Difícil (6 tentativas)");
+    string menu = Console.ReadLine();
+
+    if (int.TryParse(menu, out dificuldade))// Validar se está sendo digitado um número
+    {
+        if (dificuldade == 1)
+        {
+            vidas = 10;
+        }
+        else if (dificuldade == 2)
+        {
+            vidas = 8;
+        }
+        else if (dificuldade == 3)
+        {
+            vidas = 6;
+        }
+        else if (dificuldade != 1 && dificuldade != 2 && dificuldade != 3)
+        {
+            Console.WriteLine("Você acha que isso só é um jogo e não consegue fazer algo fácil como escolher a dificuldade,então engraçadinho bem vindo ao modo EXTREMO, onde você só tem 1 tentativa!!!!");
+            Console.ReadKey();
+            vidas = 1;
+        }
+    }
+        else
+        {
+            Console.WriteLine("Erro encontrado: Digite um NÚMERO válido!");
+            Console.WriteLine("\nPressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    
+}
 
 
 for (int i = 0; i < palavra_secreta.Length; i++) // Para escrever os "_ "
@@ -55,7 +94,7 @@ while (vidas > 0 && !ganhou) // Condição de loop do jogo ou ele perde todas as
         string letra = Console.ReadLine()?.Trim().ToUpper();
         if (string.IsNullOrEmpty(letra))
         {
-            Console.WriteLine("Nenhuma letra foi digitada!");
+            Console.WriteLine("Erro encontrado:Nenhuma letra foi digitada!");
             Console.WriteLine("\nPressione qualquer tecla para continuar");
             Console.ReadKey();
             Console.Clear();
@@ -63,7 +102,7 @@ while (vidas > 0 && !ganhou) // Condição de loop do jogo ou ele perde todas as
         }
         else if (letra.Length > 1)
         {
-            Console.WriteLine("Erro: Digite apenas UMA letra!");
+            Console.WriteLine("Erro encontrado: Digite apenas UMA letra!");
             Console.WriteLine("\nPressione qualquer tecla para continuar");
             Console.ReadKey();
             Console.Clear();
@@ -72,7 +111,7 @@ while (vidas > 0 && !ganhou) // Condição de loop do jogo ou ele perde todas as
         }
         else if (!char.IsLetter(letra[0]))
         {
-            Console.WriteLine("Erro: Digite apenas LETRAS!");
+            Console.WriteLine("Erro encontrado: Digite apenas LETRAS!");
             Console.WriteLine("\nPressione qualquer tecla para continuar");
             Console.ReadKey();
             Console.Clear();
