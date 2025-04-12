3f82e2331d1cde86f5e8c23bb5d5d7100e5f674f
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 21:36:10 -0300

    Versão_2

--- Classificação_aluno/Program.cs
+++ Classificação_aluno/Program.cs
@@ -6,6 +6,19 @@
 //3 - Calcular qual a média desses alunos
 //4 - Exibir a média
 //5 - Exibir alunos com nota acima da média
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-list<string> nome = new List<string>();
-list<double> nota = new List<double>();
+List<string> nome = new List<string>();
+List<double> nota = new List<double>();
+
+
+Console.WriteLine("Digite o nome do aluno(a):");
+nome.Add(Console.ReadLine());
+
+console.WriteLine("Digite a nota do alun(a):");
+nota.Add(double.Parse(Console.ReadLine()));
+
+Console.WriteLine($"{nomes[0]}");
+Console.WriteLine($"{notas[0]}");
