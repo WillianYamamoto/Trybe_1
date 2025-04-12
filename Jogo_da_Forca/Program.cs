af4169ba4a8cdb0f32328d4afb41e64d6e511bfc
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-12 17:00:10 -0300

    Versão_3

--- Jogo_da_Forca/Program.cs
+++ Jogo_da_Forca/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Ex Jogo da forca
 //1 - O usuário digitar somente uma letra (limitar o input para somente 1 letra)
-//2 - O programa deve mostrar a palavra como underscores (como uma palavra secreta) - em andamento
+//2 - O programa deve mostrar a palavra como underscores (como uma palavra secreta) - eOK
 // 3 - Deve revelar as letras conforme vai acertando
 // 4 - O usuário tem um número limitado de tentativas, logo deve ser mostrado ao usuário a quantidade de vidas dele 
 //6 - Melhorar programa - OK
@@ -12,5 +12,15 @@ using System.Linq;
 
 string palavra_secreta = "MOTTU";
 int palavra_underscore = palavra_secreta.Length;
+string[] letras_underscore = new string[palavra_secreta.Length];
 
 Console.WriteLine($"A palavra secreta tem:{palavra_underscore} letras");
+
+for (int i = 0; i < palavra_secreta.Length; i++) // Para escrever os "_ "
+{
+letras_underscore[i] ="-";
+
+}
+
+string palavraEscondida = string.Join(" ", letras_underscore);
+Console.WriteLine(palavraEscondida);
