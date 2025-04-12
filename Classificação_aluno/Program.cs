8a0a5a185d55a8e5a8e2def4449b48a861d816a2
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-12 16:13:30 -0300

    Versão_3

--- Classificação_aluno/Program.cs
+++ Classificação_aluno/Program.cs
@@ -7,7 +7,7 @@
 //4 - Exibir a média - OK 
 //5 - Exibir alunos com nota acima da média - OK 
 //6 - Melhorar programa - OK
-//7 - Melhorar UI/UX
+//7 - Melhorar UI/UX - OK 
 
 using System;
 using System.Collections.Generic;
@@ -16,31 +16,39 @@ using System.Linq;
 var nomes = new List<string>();
 var notas = new List<double>();
 
-double CalcularMedia(List<double> nota)
+double Calcular_Media(List<double> nota)
 {
     return nota.Count > 0 ? nota.Average() : 0;
 }
 
 string nome_novo;
-do 
+do
 {
-    Console.Write("Digite o nome do aluno(a) ou fechar para encerrar: ");
+    Console.Clear();
+    Console.Write("Digite o nome de um aluno(a) ou fechar para encerrar a lista de alunos e mostrar os alunos acima da média: ");
     nome_novo = Console.ReadLine();
 
     if (nome_novo.ToUpper() != "FECHAR")
     {
         nomes.Add(nome_novo);
         
-        Console.Write("Digite a nota do aluno(a): ");
-        if (double.TryParse(Console.ReadLine(), out double nota))
+        Console.Write("Digite a nota do aluno(a) (de o a 100): ");
+        if (double.TryParse(Console.ReadLine(), out double nota) && nota >= 0 && nota <= 100)
         {
             notas.Add(nota);
         }
         else
         {
-            Console.WriteLine("Nota inválida. Digite o nome e nota correta.");
-            nomes.RemoveAt(nomes.Count - 1); // Remove o nome adicionado
+            Console.WriteLine("Nota inválida. Digite NOVAMENTE o nome e nota correta (Entre 0 e 100).");
+            nomes.RemoveAt(nomes.Count - 1); // Remove o nome adicionado, caso o número seja inválido
+            Console.ReadKey();
             continue;
+        }
+        if (nome_novo.ToUpper() != "FECHAR") // Para deixar a UI/UX melhor
+        {
+            Console.WriteLine("\nPressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 } while (nome_novo.ToUpper() != "FECHAR");
@@ -51,7 +59,7 @@ for (int i = 0; i < nomes.Count; i++)
     Console.WriteLine($"{nomes[i]} - Nota: {notas[i]}");
 }
 
-double media = CalcularMedia(notas);
+double media = Calcular_Media(notas); // calcula a média 
 Console.WriteLine($"\nMédia da turma: {media}");
 
 Console.WriteLine("\nAlunos com nota acima da média:");
---
+++ Jogo_da_Forca/Program.cs
@@ -0,0 +1,5 @@
+﻿//Ex Jogo da forca
+//1 - O usuário digitar somente uma letra (limitar o input para somente 1 letra)
+//2 - O programa deve mostrar a palavra como underscores (como uma palavra secreta)
+// 3 - Deve revelar as letras conforme vai acertando
+// 4 - O usuário tem um número limitado de tentativas, logo deve ser mostrado ao usuário a quantidade de vidas dele 
