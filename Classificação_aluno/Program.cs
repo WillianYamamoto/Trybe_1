9893658660ac38961b4c7e1224e5c0aff5c2b910
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-11 21:49:55 -0300

    Versão_2

--- Classificação_aluno/Program.cs
+++ Classificação_aluno/Program.cs
@@ -1,8 +1,8 @@
 ﻿//Crie uma lista que pega o nome e nota dos alunos
 //O programa deve calcular a média da turma e exibir as pessoas com nota acima da média 
 // Etapas:
-//1 - Crie uma lista que pega o nome dos alunos
-//2 - Crie uma lista qeu pega as notas dos alunos
+//1 - Crie uma lista que pega o nome dos alunos - OK
+//2 - Crie uma lista qeu pega as notas dos alunos - OK
 //3 - Calcular qual a média desses alunos
 //4 - Exibir a média
 //5 - Exibir alunos com nota acima da média
@@ -10,15 +10,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-List<string> nome = new List<string>();
-List<double> nota = new List<double>();
+ var nomes = new List<string>();
+var notas = new List<double>();
 
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Digite o nome do aluno(a): ");
+    nomes.Add(Console.ReadLine());
 
-Console.WriteLine("Digite o nome do aluno(a):");
-nome.Add(Console.ReadLine());
+    Console.Write("Digite a nota do aluno(a): ");
+    notas.Add(double.Parse(Console.ReadLine()));
+}
 
-console.WriteLine("Digite a nota do alun(a):");
-nota.Add(double.Parse(Console.ReadLine()));
-
-Console.WriteLine($"{nomes[0]}");
-Console.WriteLine($"{notas[0]}");
+Console.WriteLine("\nAlunos e notas:");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"{nomes[i]} - Nota: {notas[i]}");
+}
