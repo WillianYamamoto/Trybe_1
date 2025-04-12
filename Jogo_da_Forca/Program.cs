1ddf060d3c1d1d4ce8edeaecc4fe48c6252c94f0
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-12 18:49:22 -0300

    Versão_3

--- Jogo_da_Forca/Program.cs
+++ Jogo_da_Forca/Program.cs
@@ -15,7 +15,6 @@ char[] letras_underscore = new char[palavra_secreta.Length];
 int vidas = 5;
 bool ganhou = false;
 
-Console.WriteLine($"A palavra secreta tem:{palavra_secreta.Length} letras");
 
 for (int i = 0; i < palavra_secreta.Length; i++) // Para escrever os "_ "
 {
@@ -23,15 +22,15 @@ for (int i = 0; i < palavra_secreta.Length; i++) // Para escrever os "_ "
 }
 
 string palavraEscondida = string.Join(" ", letras_underscore); // para colocar na tela a palavra escondida
-//Console.WriteLine(palavraEscondida);
-char chute;
 
 while (vidas > 0 && !ganhou)
 {
     Console.WriteLine($"A palavra secreta tem:{palavra_secreta.Length} letras");
     Console.WriteLine("\n" + string.Join(" ", letras_underscore));
     Console.WriteLine($"Tentativas restantes: {vidas}");
-    while (true)
+
+    char chute;
+    while (true)// Para validar que está sendo digitado somente 1 letra
     {
         Console.WriteLine("Digite uma letra:");
         string letra = Console.ReadLine()?.Trim().ToUpper();
@@ -40,7 +39,7 @@ while (vidas > 0 && !ganhou)
             Console.WriteLine("Nenhuma letra foi digitada!");
             Console.WriteLine("\nPressione qualquer tecla para continuar");
             Console.ReadKey();
-            Console.Clear();
+           
             continue;
         }
         else if (letra.Length > 1)
@@ -48,7 +47,7 @@ while (vidas > 0 && !ganhou)
             Console.WriteLine("Erro: Digite apenas UMA letra!");
             Console.WriteLine("\nPressione qualquer tecla para continuar");
             Console.ReadKey();
-            Console.Clear();
+            
             continue;
         }
         else if (!char.IsLetter(letra[0]))
@@ -56,7 +55,7 @@ while (vidas > 0 && !ganhou)
             Console.WriteLine("Erro: Digite apenas LETRAS!");
             Console.WriteLine("\nPressione qualquer tecla para continuar");
             Console.ReadKey();
-            Console.Clear();
+           
             continue;
         }
 
@@ -65,13 +64,23 @@ while (vidas > 0 && !ganhou)
     }
     ;
 
+    bool letraDigitada = false;
     for (int i = 0; i < palavra_secreta.Length; i++)
     {
         if (palavra_secreta[i] == chute)
         {
             letras_underscore[i] = chute;
+            letraDigitada = true;
         }
     }
+    ;
+    if (!letraDigitada)
+    {
+        vidas--;
+        Console.WriteLine($"A letra {chute} não está na palavra!");
+    }
+
+
 }
 palavraEscondida = string.Join(" ", letras_underscore);
 Console.WriteLine(palavraEscondida);
