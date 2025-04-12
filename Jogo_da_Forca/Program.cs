dc19c2741821ec71455d1a966e41a06e00ee8792
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-12 18:31:40 -0300

    Versão_3

--- Jogo_da_Forca/Program.cs
+++ Jogo_da_Forca/Program.cs
@@ -12,7 +12,8 @@ using System.Linq;
 
 string palavra_secreta = "MOTTU";
 char[] letras_underscore = new char[palavra_secreta.Length];
-
+int vidas = 5;
+bool ganhou = false;
 
 Console.WriteLine($"A palavra secreta tem:{palavra_secreta.Length} letras");
 
@@ -21,39 +22,55 @@ for (int i = 0; i < palavra_secreta.Length; i++) // Para escrever os "_ "
     letras_underscore[i] = '_';
 }
 
-string palavraEscondida = string.Join(" ", letras_underscore);
-Console.WriteLine(palavraEscondida);
+string palavraEscondida = string.Join(" ", letras_underscore); // para colocar na tela a palavra escondida
+//Console.WriteLine(palavraEscondida);
 char chute;
 
-while (true)
+while (vidas > 0 && !ganhou)
 {
-Console.WriteLine("Digite uma letra:");
-string letra = Console.ReadLine()?.Trim().ToUpper();    
-if (string.IsNullOrEmpty(letra))
-{
-    Console.WriteLine("Nenhuma letra foi digitada!");
-    continue;
-}
-else if (letra.Length > 1)
-{
-    Console.WriteLine("Erro: Digite apenas UMA letra!");
-    continue;
-}
-else if (!char.IsLetter(letra[0]))
-{
-    Console.WriteLine("Erro: Digite apenas LETRAS!");
-    continue;
-}
-
-chute = letra[0];
-break;
-};
-
-for (int i = 0; i < palavra_secreta.Length; i++)
-{
-    if (palavra_secreta[i] == chute)
+    Console.WriteLine($"A palavra secreta tem:{palavra_secreta.Length} letras");
+    Console.WriteLine("\n" + string.Join(" ", letras_underscore));
+    Console.WriteLine($"Tentativas restantes: {vidas}");
+    while (true)
     {
-        letras_underscore[i] = chute;
+        Console.WriteLine("Digite uma letra:");
+        string letra = Console.ReadLine()?.Trim().ToUpper();
+        if (string.IsNullOrEmpty(letra))
+        {
+            Console.WriteLine("Nenhuma letra foi digitada!");
+            Console.WriteLine("\nPressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+            continue;
+        }
+        else if (letra.Length > 1)
+        {
+            Console.WriteLine("Erro: Digite apenas UMA letra!");
+            Console.WriteLine("\nPressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+            continue;
+        }
+        else if (!char.IsLetter(letra[0]))
+        {
+            Console.WriteLine("Erro: Digite apenas LETRAS!");
+            Console.WriteLine("\nPressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+            continue;
+        }
+
+        chute = letra[0];
+        break;
+    }
+    ;
+
+    for (int i = 0; i < palavra_secreta.Length; i++)
+    {
+        if (palavra_secreta[i] == chute)
+        {
+            letras_underscore[i] = chute;
+        }
     }
 }
 palavraEscondida = string.Join(" ", letras_underscore);
