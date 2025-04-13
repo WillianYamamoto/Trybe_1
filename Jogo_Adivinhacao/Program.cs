8b25f5096d3e13ba82a1164194a3e1f5b1623085
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-13 10:35:59 -0300

    Versão_4

---
+++ Jogo_Adivinhacao/Program.cs
@@ -0,0 +1,7 @@
+﻿// Jodo da adivinhação com dicas precisa 
+// 1 - o usuário deve chutar um número de 1 a 1000
+// 2 - após cada tentativa deve retornar se o número misterioso é maior ou menor que o chute 
+// 3 - Também deve informar se ele está muito distante (diferença>100), distante (diferença >50) ou perto (diferença <= 50)   
+// detalhes: Não tem um número exato de tentativas que ele deve ter
+//5 - Melhorar programa  - OK
+//6 - Melhorar UI/UX  - OK
