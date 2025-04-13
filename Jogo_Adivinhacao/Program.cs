9289ad3b2b5af8adfe8a80dc76fa8edf2fc1bb29
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-04-13 10:41:50 -0300

    Versão_4

--- Jogo_Adivinhacao/Program.cs
+++ Jogo_Adivinhacao/Program.cs
@@ -3,5 +3,11 @@
 // 2 - após cada tentativa deve retornar se o número misterioso é maior ou menor que o chute 
 // 3 - Também deve informar se ele está muito distante (diferença>100), distante (diferença >50) ou perto (diferença <= 50)   
 // detalhes: Não tem um número exato de tentativas que ele deve ter
-//5 - Melhorar programa  - OK
-//6 - Melhorar UI/UX  - OK
+//5 - Melhorar programa 
+//6 - Melhorar UI/UX  
+
+
+Random random = new Random();
+int num_secreto = random.Next(1,1001);// Sorte um número entre 1 e 1000
+
+Console.WriteLine($"O número secreto é: {num_secreto}");
